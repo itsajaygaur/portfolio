cf1e015d0cfb3b954e576b020a0b9b06e992cd91
ajay <ajaypathak2527@gmail.com>
2024-04-30 11:29:08 +0530

    add github and linkedin

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,26 @@
 import ThemeToggler from "./ThemeToggler"
+import { FaGithub } from "react-icons/fa";
+import { SiLinkedin } from "react-icons/si";
+import Link from "next/link";
+import { Button } from "@nextui-org/react";
 
 
 export default function Navbar(){
     return(
-        <section className='text-end p-6' >
+        <section className='flex justify-between px-6 py-4' >
+            <div className="flex items-center gap-2" >
+                <Button variant="light" isIconOnly >
+                    <Link target="_blank" href="https://github.com/itsajaygaur" >
+                        <FaGithub size={24} />
+                    </Link>
+                </Button>
+                <Button variant="light" isIconOnly >
+
+                <Link target="_blank" href="https://www.linkedin.com/in/ajay-gaur-70a4951ab/" >
+                    <SiLinkedin size={24} />
+                </Link>
+                </Button>
+            </div>
             <ThemeToggler />
         </section>
     )
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -53,7 +53,8 @@ export default function Projects() {
 
   return (
     <section className="mb-20 md:mb-40" >
-        <h2 className="text-3xl mb-4 text-center" >My Work</h2>
+        <h2 className="text-3xl text-center" >My Work</h2>
+        <p className="text-center mb-4" >Some of the projects are from work and some are on my own time.</p>
         <div className="gap-2 sm:gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3" >
 
       {projects.map((item, index) => (
@@ -66,7 +67,7 @@ export default function Projects() {
               width={1920}
               height={1080}
               alt={item.title}
-              className="w-full object-cover aspect-video grayscale hover:grayscale-0 transition hover:scale-105 "
+              className="w-full object-cover aspect-video sm:grayscale sm:hover:grayscale-0 transition hover:scale-105 "
               src={item.img}
             />
           </CardBody>
