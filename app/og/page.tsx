d9b435b3c1d893c975f81a3e0bd0305e5a4a19d8
ajay <ajaypathak2527@gmail.com>
2024-05-11 13:01:01 +0530

    og image heading text white

--- app/og/page.tsx
+++ app/og/page.tsx
@@ -1,7 +1,7 @@
 export default function Og(){
     return(
         <section className="h-screen w-screen flex flex-col justify-center items-center bg-gradient-to-r from-black  to-zinc-900" >
-            <h1 className="text-6xl mb-10" >Ajay Gaur</h1>
+            <h1 className="text-6xl text-white mb-10" >Ajay Gaur</h1>
             <p className="text-4xl text-zinc-500" > Full Stack Developer </p>
         </section>
     )
