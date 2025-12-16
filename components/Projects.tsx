06e8b4f0734929747b1d98061ff75715881d18f3
ajay <ajaypathak2527@gmail.com>
2025-12-16 23:55:06 +0530

    build fix

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,6 +1,6 @@
-import {Card, CardBody, CardFooter} from "@heroui/react";
+// import {Card, CardBody, CardFooter} from "@heroui/react";
 import Image from "next/image";
-import { Link } from "@heroui/react";
+// import { Link } from "@heroui/react";
 import { FaGithub } from "react-icons/fa";
 import { FaExternalLinkSquareAlt } from "react-icons/fa";
 import { projects } from "@/constants/data";
@@ -17,35 +17,34 @@ export default function Projects() {
         <div className="gap-2 sm:gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3" >
 
       {projects.map((item) => (
-          <Card shadow="lg" key={item.id} isPressable={false} >
-          <CardBody className="overflow-visible p-0">
-            <Image
-            //   shadow="sm"
-            //   radius="lg"
-              width={1920}
-              height={1080}
-              alt={item.title}
-              className="w-full object-cover aspect-video sm:grayscale sm:hover:grayscale-0 transition hover:scale-105 "
-              src={item.img}
-            />
-          </CardBody>
-          <CardFooter className="text-small flex-col gap-1 items-start">
-            <b>{item.title}</b>
-            <div className="flex gap-6 items-center" >
-              <Link underline="hover" href={item.link} target="_blank" className="text-blue-500 dark:text-blue-500" >
-                <FaExternalLinkSquareAlt className="mr-1" />
-                View live
-              </Link>
-              {
-                item.github &&
-                <Link underline="hover" href={item?.github} target="_blank" className="text-blue-500" >
-                <FaGithub className="mr-1" />
-                View code
-              </Link>
-              }
-            </div>
-          </CardFooter>
-        </Card>
+        null
+        //   <Card shadow="lg" key={item.id} isPressable={false} >
+        //   <CardBody className="overflow-visible p-0">
+        //     <Image
+        //       width={1920}
+        //       height={1080}
+        //       alt={item.title}
+        //       className="w-full object-cover aspect-video sm:grayscale sm:hover:grayscale-0 transition hover:scale-105 "
+        //       src={item.img}
+        //     />
+        //   </CardBody>
+        //   <CardFooter className="text-small flex-col gap-1 items-start">
+        //     <b>{item.title}</b>
+        //     <div className="flex gap-6 items-center" >
+        //       <Link underline="hover" href={item.link} target="_blank" className="text-blue-500 dark:text-blue-500" >
+        //         <FaExternalLinkSquareAlt className="mr-1" />
+        //         View live
+        //       </Link>
+        //       {
+        //         item.github &&
+        //         <Link underline="hover" href={item?.github} target="_blank" className="text-blue-500" >
+        //         <FaGithub className="mr-1" />
+        //         View code
+        //       </Link>
+        //       }
+        //     </div>
+        //   </CardFooter>
+        // </Card>
       ))}
             </div>
     </section>
