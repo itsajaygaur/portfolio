19e25a580a212b11331089b30171e328b1fd3067
ajay <ajaypathak2527@gmail.com>
2025-12-16 23:41:06 +0530

    update skills, resume, project & design, fix react cve security issue,

--- types/zod-schemas.ts
+++ types/zod-schemas.ts
@@ -2,6 +2,6 @@ import {z} from "zod"
 
 export const contactFormSchema = z.object({
     name: z.string().trim().min(1, {message: 'This field is required.'}),
-    email: z.string().email(),
+    email: z.email().trim().min(1, {message: 'This field is required.'}),
     message: z.string().trim().min(1, {message: 'This field is required.'})
 })
