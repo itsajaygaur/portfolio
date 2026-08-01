e092d306451bfcb29cd6cb65cace83b1d2deec82
itsajaygaur <ajaypathak2527@gmail.com>
2026-08-01 15:40:49 +0530

    Upgrade dependencies and remove dead code

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypeScript from "eslint-config-next/typescript";
+
+export default defineConfig([
+  ...nextCoreWebVitals,
+  ...nextTypeScript,
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
